923c2551d80456d13b469da20caac34442113539
lihao <lihao@zego.im>
2022-10-31 15:44:59 +0800

    📝 Remove redundant param

--- src/core/defines.js
+++ src/core/defines.js
@@ -1,7 +1,7 @@
 export default class ZegoPluginResult {
   code = '';
   message = '';
-  constructor(code = '', message = '', result = '') {
+  constructor(code = '', message = '') {
     this.code = code;
     this.message = message;
   }
